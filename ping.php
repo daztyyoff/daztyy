d503f869b637758f952ee36c946de5e12b57add8
Daztyy <158313784+daztyyoff@users.noreply.github.com>
2024-12-05 21:52:13 +0400

    Update ping 0.1 .php

--- ping.php
+++ ping.php
@@ -1,35 +1,14 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $token = 8086168051:AAFFb4uqsGyK3tAHYifeFwjd17ZlGZ88p6Y;  // Замените на ваш токен
-    $chat_id = 1085992226;    
-    $message = "Кто-то нажал на кнопку 'Пинг!' на сайте.";  // Сообщение
+// Ваш токен и chat_id
+$bot_token = "8086168051:AAFFb4uqsGyK3tAHYifeFwjd17ZlGZ88p6Y";
+$chat_id = "1085992226";
 
-    // URL для отправки запроса
-    $url = "https://api.telegram.org/bot{$token}/sendMessage";
+// Сообщение для отправки
+$message = "Пинг!";
 
-    // Параметры запроса
-    $data = [
-        'chat_id' => $chat_id,
-        'text' => $message
-    ];
+// Формируем URL для запроса к API Telegram
+$api_url = "https://api.telegram.org/bot$bot_token/sendMessage?chat_id=$chat_id&text=" . urlencode($message);
 
-    // Отправка запроса
-    $options = [
-        'http' => [
-            'header'  => "Content-type: application/x-www-form-urlencoded\r\n",
-            'method'  => 'POST',
-            'content' => http_build_query($data),
-        ],
-    ];
-    $context  = stream_context_create($options);
-    $result = file_get_contents($url, false, $context);
-
-    if ($result) {
-        echo "Пинг отправлен в Telegram!";
-    } else {
-        echo "Ошибка при отправке пинга.";
-    }
-} else {
-    echo "Неверный запрос.";
-}
+// Отправляем запрос
+file_get_contents($api_url);
 ?>
